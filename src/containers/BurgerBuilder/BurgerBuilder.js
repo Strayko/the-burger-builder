efb3e881448ec6e44d7bbe3754acbbfdd9006472
Strayko <strayko_lol@windowslive.com>
2019-02-05 14:09:16 +0100

    adding a toolbar

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -80,6 +80,10 @@ purchaseCancelHandler = () => {
   this.setState({purchasing: false});
 }
 
+purchaseContinueHandler = () => {
+  alert('You continue!');
+}
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -91,7 +95,11 @@ purchaseCancelHandler = () => {
     return(
       <Aux>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary 
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCanceled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls 
