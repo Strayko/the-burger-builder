b83ee8a91506413ec810f281a0e81546b2b4a35a
Strayko <strayko_lol@windowslive.com>
2019-02-25 20:14:53 +0100

    order submission & passing data between pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,33 +92,13 @@ purchaseCancelHandler = () => {
 }
 
 purchaseContinueHandler = () => {
-  // this.setState({loading: true});
   // // alert('You continue!');
-  // const order = {
-  //   ingredients: this.state.ingredients,
-  //   price: this.state.totalPrice,
-  //   customer: {
-  //     name: 'Moamer Jusupovic',
-  //     address: {
-  //       street: 'Teststreet 1',
-  //       zipCode: '43567',
-  //       country: 'Finland'
-  //     },
-  //     email: 'moamer@live.com'
-  //   },
-  //   deliveryMethod: 'fastest'
-  // }
-  // axios.post('/orders.json', order)
-  //   .then(response => {
-  //     this.setState({loading: false, purchasing: false});
-  //   })
-  //   .catch(error => {
-  //     this.setState({loading: false, purchasing: false});
-  //   });
+  
   const queryParams = [];
   for(let i in this.state.ingredients) {
     queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
   }
+  queryParams.push('price=' + this.state.totalPrice);
   const queryString = queryParams.join('&');
   this.props.history.push({
     pathname: '/checkout',
