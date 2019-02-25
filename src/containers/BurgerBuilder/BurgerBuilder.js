a33d98be7ee5c9a136419444092f85f7d189e300
Strayko <strayko_lol@windowslive.com>
2019-02-25 16:27:03 +0100

    navigating back & to next page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,6 +31,7 @@ state = {
 }
 
 componentDidMount() {
+  console.log(this.props);
   axios.get('https://the-burger-builder-5800f.firebaseio.com/ingredients.json')
     .then(response => {
       this.setState({ingredients: response.data});
@@ -91,29 +92,30 @@ purchaseCancelHandler = () => {
 }
 
 purchaseContinueHandler = () => {
-  this.setState({loading: true});
-  // alert('You continue!');
-  const order = {
-    ingredients: this.state.ingredients,
-    price: this.state.totalPrice,
-    customer: {
-      name: 'Moamer Jusupovic',
-      address: {
-        street: 'Teststreet 1',
-        zipCode: '43567',
-        country: 'Finland'
-      },
-      email: 'moamer@live.com'
-    },
-    deliveryMethod: 'fastest'
-  }
-  axios.post('/orders.json', order)
-    .then(response => {
-      this.setState({loading: false, purchasing: false});
-    })
-    .catch(error => {
-      this.setState({loading: false, purchasing: false});
-    });
+  // this.setState({loading: true});
+  // // alert('You continue!');
+  // const order = {
+  //   ingredients: this.state.ingredients,
+  //   price: this.state.totalPrice,
+  //   customer: {
+  //     name: 'Moamer Jusupovic',
+  //     address: {
+  //       street: 'Teststreet 1',
+  //       zipCode: '43567',
+  //       country: 'Finland'
+  //     },
+  //     email: 'moamer@live.com'
+  //   },
+  //   deliveryMethod: 'fastest'
+  // }
+  // axios.post('/orders.json', order)
+  //   .then(response => {
+  //     this.setState({loading: false, purchasing: false});
+  //   })
+  //   .catch(error => {
+  //     this.setState({loading: false, purchasing: false});
+  //   });
+  this.props.history.push('/checkout');
 }
 
   render() {
