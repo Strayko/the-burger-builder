63e02c0eac214fff1cd76b1a1cc607f47a7038b0
Strayko <strayko_lol@windowslive.com>
2019-02-28 14:20:50 +0100

    form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,7 +19,8 @@ state = {
       validation: {
         required: true
       },
-      valid: false
+      valid: false,
+      touched: false
     },
     street: {
       elementType: 'input',
@@ -31,7 +32,8 @@ state = {
       validation: {
         required: true
       },
-      valid: false
+      valid: false,
+      touched: false
     },
     zipCode: {
       elementType: 'input',
@@ -45,7 +47,8 @@ state = {
         minLength: 5,
         maxLength: 5
       },
-      valid: false
+      valid: false,
+      touched: false
     },
     country: {
       elementType: 'input',
@@ -57,7 +60,8 @@ state = {
       validation: {
         required: true
       },
-      valid: false
+      valid: false,
+      touched: false
     },
     email: {
       elementType: 'input',
@@ -69,7 +73,8 @@ state = {
       validation: {
         required: true
       },
-      valid: false
+      valid: false,
+      touched: false
     },
     deliveryMethod: {
       elementType: 'select',
@@ -79,9 +84,12 @@ state = {
               {value: 'cheapest', displayValue: 'Cheapest'}
           ]
       },
-      value: ''
+      value: '',
+      validation: {},
+      valid: true
     }
   },
+  formIsValid: false,
   loading: false
 }
 
@@ -108,18 +116,21 @@ orderHandler = ( event ) => {
 }
 
 checkValidity(value, rules) {
-  let isValid = false;
+  let isValid = true;
+  if(!rules) {
+    return true;
+  }
   
   if(rules.required) {
-    isValid = value.trim() !== '';
+    isValid = value.trim() !== '' && isValid;
   }
 
   if(rules.minLength) {
-    isValid = value.length >= rules.minLength
+    isValid = value.length >= rules.minLength && isValid
   }
 
   if(rules.maxLength) {
-    isValid = value.length <= rules.maxLength
+    isValid = value.length <= rules.maxLength && isValid
   }
 
   return isValid;
@@ -134,9 +145,14 @@ inputChangedHandler = (event, inputIdentifier) => {
   };
   updatedFormElement.value = event.target.value;
   updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+  updatedFormElement.touched = true;
   updatedOrderForm[inputIdentifier] = updatedFormElement;
-  console.log(updatedFormElement);
-  this.setState({orderForm: updatedOrderForm});
+  
+  let formIsValid = true;
+  for(let inputIdentifier in updatedOrderForm) {
+    formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+  }
+  this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
 }
 
 render () {
@@ -155,9 +171,12 @@ render () {
           elementType={formElement.config.elementType}
           elementConfig={formElement.config.elementConfig}
           value={formElement.config.value}
+          invalid={!formElement.config.valid}
+          shouldValidate={formElement.config.validation}
+          touched={formElement.config.touched}
           changed={(event) => this.inputChangedHandler(event, formElement.id)} />
       ))}
-      <Button btnType="Success">ORDER</Button>
+      <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
     </form>
   );
   if ( this.state.loading ) {
