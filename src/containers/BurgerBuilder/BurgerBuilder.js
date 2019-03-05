50c443b6a107d81f85892ebb3c1182f6fcc597bc
Strayko <strayko_lol@windowslive.com>
2019-03-05 17:28:42 +0100

    redux & ui state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,6 @@ class BurgerBuilder extends Component {
 //   this.state = {...}
 // }
 state = {
-  purchaseable: false,
   purchasing: false,
   loading: false,
   error: false
@@ -43,7 +42,7 @@ updatePurchaseState (ingredients) {
     .reduce((sum, el) => {
       return sum + el;
     }, 0);
-  this.setState({purchaseable: sum > 0});
+  return sum > 0;
 }
 
 purchaseHandler = () => {
@@ -89,7 +88,7 @@ purchaseContinueHandler = () => {
           ingredientAdded={this.props.onIngredientAdded}
           ingredientRemove={this.props.onIngredientRemoved}
           disabled={disabledInfo}
-          purchaseable={this.state.purchaseable}
+          purchaseable={this.updatePurchaseState(this.props.ings)}
           ordered={this.purchaseHandler}
           price={this.props.price} />
           </Aux>
