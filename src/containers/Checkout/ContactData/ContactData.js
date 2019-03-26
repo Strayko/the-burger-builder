afdfd3cb4527bc6a327364567ec5d85f98bc8326
Strayko <strayko_lol@windowslive.com>
2019-03-26 21:11:31 +0100

    combining reducers

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -87,7 +87,7 @@ state = {
               {value: 'cheapest', displayValue: 'Cheapest'}
           ]
       },
-      value: '',
+      value: 'fastest',
       validation: {},
       valid: true
     }
@@ -187,9 +187,9 @@ render () {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice,
-    loading: state.loading
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.order.loading
   }
 }
 
